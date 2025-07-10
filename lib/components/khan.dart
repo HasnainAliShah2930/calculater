7315a79da77c13dd2b7da72859f087329cb21c18
HasnainAliShah2930 <hasnainalishahg2930@gmail.com>
2025-07-10 03:43:54 -0700

    i have add khan.

---
+++ lib/components/khan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Khan extends StatefulWidget {
+  const Khan({super.key});
+
+  @override
+  State<Khan> createState() => _KhanState();
+}
+
+class _KhanState extends State<Khan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
